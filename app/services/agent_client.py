ed0f65e862e4ea8ee842c33793684d740e387650
yadhiraflores109 <yadhiraflores109@gmail.com>
2025-11-22 17:58:06 -0500

    new code controllere for main

--- app/services/agent_client.py
+++ app/services/agent_client.py
@@ -8,7 +8,7 @@ class AgentClient:
     Cliente para comunicarse con el Agente Executor de Selenium.
     """
     def __init__(self):
-        self.executor_url = os.getenv("AGENT_EXECUTOR_URL", "http://localhost:8001/execute")
+        self.executor_url = os.getenv("AGENT_EXECUTOR_URL", "http://localhost:8000/execute")
     
     def extract_python_code(self, text: str) -> str:
         """
